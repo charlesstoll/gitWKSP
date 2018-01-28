cdbda922af65561bdc9f77ae7ca1d57e269676e5
charlesstoll <charlesrstoll@gmail.com>
2018-01-28 10:13:03 -0800

    Fast commit from Someone Else this commit was made while I was making changes too.

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main(int argc, char* argv[])
 	printf("initial commit of files\n");
 	printf("good commit\n");
 	printf("commit from someone else\n");
+	printf("commit from someone else before I could commit\n");
 	return 0;
 }
