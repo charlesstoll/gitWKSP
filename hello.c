96a9c714d4f72366fa3739b304110ba86199ff87
charlesstoll <charlesrstoll@gmail.com>
2018-01-28 10:26:22 -0800

    branch 1 first commit this is a first commit for a new branch

--- hello.c
+++ hello.c
@@ -7,5 +7,6 @@ int main(int argc, char* argv[])
 	printf("commit from someone else\n");
 	printf("My newest commit\n");
 	printf("commit from someone else before I could commit and I made a change\n");
+	printf("First commit for branch 1\n");
 	return 0;
 }
